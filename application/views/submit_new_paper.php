b4105dd277c2e18b50c0e3b169f81e848c4e289d
BBackerry <falah.salim@gmail.com>
2013-11-18 14:43:08 -0500

    Fixed DB model foreign key names

--- application/views/submit_new_paper.php
+++ application/views/submit_new_paper.php
@@ -70,13 +70,64 @@
     <div class="container">
       <!-- Example row of columns -->
       <div class="row">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<h3>Submit New Paper:</h3>
+				<p>Paper Info<p>
+				<p>Uploading paper period is from 2012-07-15 to 2013-09-27<p>
+				<p>You can only change the Title, Abstract, Authors, or paper file during this period. No author can be added or deleted after this period.<p>
+				<p>Fields with * are required.<p>
+				<p>The abstract must be at least 20 words, but no more than 200 characters including blank spaces. You can write the abstract in a text editor then copy and paste it in the appropriate field.<p>
+			  
+				
+				<form role="form" class="form-horizontal">
+					<div class="form-group">
+						<label for="title" class="col-lg-2 control-label">*Paper Title:</label>
+						<div class="col-lg-10">
+							<input type="text" class="form-control" id="title">
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<label for="abstract" class="col-lg-2 control-label">*Paper Abstract:</label>
+						<div class="col-lg-10">
+							<input type="text" class="form-control" id="abstract">
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<label for="file" class="col-lg-2 control-label">*Paper File(PDF):</label>
+						<div class="col-lg-10">
+							<input type="text" class="form-control" id="file">
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<label for="keywords" class="col-lg-2 control-label">*Keywords:</label>
+						<div class="col-lg-10">
+							<input type="text" class="form-control" id="keywords">
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<label for="subject" class="col-lg-2 control-label">*Paper Subject:</label>
+						<div class="col-lg-10">
+							<div class="checkbox">
+								<label><input type="checkbox">Subject 1</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox">Subject 2</label>
+							</div>
+						</div>
+					</div>
+					
+					<a href="#" class="btn btn-success" style="float:right;">Submit</a>
+				</form>
+			  
+			</div>
+		</div>
+
         <div class="col-lg-12">
-			<form role="form" class="form-horizontal">
-				<div class="form-group">
-					<label for="title" class="col-lg-2 control-label">Paper Title:</label>
-					<input type="text" name="title" id="title"/>
-				</div>
-			</form>
         </div>
       </div>
 
