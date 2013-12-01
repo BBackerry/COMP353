8583a4bef38e83badcc08d2592d69b565f531d4a
Shurouq <sh.abusalah@gmail.com>
2013-12-01 15:27:01 -0500

    create event - not sure if it's working .. waiting for the database to be fixed

--- application/controllers/Event.php
+++ application/controllers/Event.php
@@ -1,47 +1,53 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Event extends CI_Controller {
+
+public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('event_model');
+	}
 	
 	public function addEvent()
 	{
-		$this->load->view('header');
-		$this->load->view('add_new_event');
-	}
-    
-    public function submit()
-	{
-		$this->load->view('header');
-		$this->load->view('event_page');
+			$username = $this->session->userdata('idUser');
+			$query['roles'] = $this->role_model->get_role_of_user($username);
+			$admin = false;
+			foreach ($query->result() as $row)
+			{
+				if($row->idPosition == 1 & $row->idEvent == 1)
+				$admin = true;
+			}
+			if ($admin)
+			{
+			$this->load->view('header');
+			$this->load->view('add_new_event');
+			}
+		else 
+		{
+			$errors['errorMessagesEvent'] = array("Sorry but you are no allowed to create event");
+			$this->load->view('header', $errors);
+			$this->load->view('home_page');
+		}
 	}
 	
-	public function addedEvent()
+     
+	public function submitedEvent()
 	{
-		$eventTitle = $this->input->get('eventTitle');
-		$eventDescription = $this->input->get('eventDescription');
-		$idTopicHierarcy = $this->input->get('idTopicHierarchy');
+		$username = $this->session->userdata('idUser');
+		
+		$eventName = $this->input->get('eventName');
+		$eventDescription = $this->input->get('eventDescription');		
 		$startDate = $this->input->get('startDate');
 		$endDate = $this->input->get('endDate');
-		$schedulingTemplate = $this->input->get('schedulingTemplate');
-		$copyGroupsFrom = $this->input->get('copyGroupsFrom');
-		$requestFullPaper = $this->input->get('requestFullPaper');
-		$requestAbstract = $this->input->get('requestAbstract');
-		$enableRegistration = $this->input->get('enableRegistration');
-		$enablePaperAuction = $this->input->get('enablePaperAuction');
-		$enablePaperReview = $this->input->get('enablePaperReview');
-		$enableAutoReviewAllocation = $this->input->get('enableAutoReviewAllocation');
-		$enableBlindDebate = $this->input->get('enableBlindDebate');
-		$requestPublisherCpoyright = $this->input->get('requestPublisherCpoyright');
-		$upload = $this->input->get('upload');
-		$inline = $this->input->get('inline');
-		$online = $this->input->get('online');
-		$requestCIDI = $this->input->get('requestCIDI');
-		$upload1 = $this->input->get('upload1');
-		$inline1 = $this->input-get('inline1');
-		$online1 = $this->input->get('online1');
-		$requestFinal = $this->input->get('requestFinal');
-		$requestSlide = $this->input->get('requestSlide');
-		$enablePresentation = $this->input->get('enablePresentation');
-		$checkFonts = $this->input->get('checkFonts');
+					
+		$this->event_model->create_event($eventName, $startDate, $endDate, $username);
 			
+		$this->load->view('header');
+		$this->load->view('event_page');		
+		
 	}
+		
+			
+	
 }
--- application/views/add_new_event.php
+++ application/views/add_new_event.php
@@ -25,8 +25,7 @@
         <option value="??">??</option>
         </select>
               
-        </div>
-       
+        </div>      
         
          
          <div class="form-group">
@@ -38,63 +37,8 @@
         <p>End Date: </p>
         <input type="text" class="form-control" name ="endDate" id="endDate">        
         </div>
-        
-        
-        
-        <div class="form-group">
-        <p>Scheduling Template </p>
-        <select class="form-control" id="schedulingTemplate" name="schedulingTemplate">
-        <option value = "Use the system default">Use the system default</option>
-        <option value="??">??</option>
-        </select>
-                
-        </div>
-        
-        
-        
-        <div class="form-group">
-        <p>Copy Groups From </p>
-        <select class="form-control" id = "copyGroupsFrom" name="copyGroupsFrom">
-        <option value = "don't copy">Don't copy</option>
-        <option value="??">??</option>
-        </select>
-                
-        </div>
-        
-        
-        
-        
-        <div class="form-group">
-        <p>Choose Event Parameter </p>
-        
-        <div class="col-lg-6">
-       
-        <input type="radio" name="requestFullPaper" value="Request Full Paper" id="requestFullPaper"> Request Full Paper <br>
-    
-        <input type="radio" name="requestAbstract" value="Request Abstract" id="requestAbstract"> Request Abstract<br>
-      
-       <input type="checkbox" name="enableRegistration" value="Enable Registration Control" id="enableRegistration">Enable Registration Control<br>
-        <input type="checkbox" name="enablePaperAuction" value="Enable Paper Auction" id="enablePaperAuction">Enable Paper Auction<br>
-        <input type="checkbox" name="enablePaperReview" value="Enable Paper Review" id="enablePaperReview">Enable Paper Review<br>
-        <input type="checkbox" name="enableAutoReviewAllocation" value="Enable Auto Review Allocation" id="enableAutoReviewAllocation">Enable Auto Review Allocation<br>
-        <input type="checkbox" name="enableBlindDebate" value="Enable Blind Debate" id="enableBlindDebate">Enable Blind Debate<br>
-        <input type="checkbox" name="requestPublisherCpoyright" value="Request Publisher Copyright Form" id="requestPublisherCpoyright">Request Publisher Copyright Form<br>
-            
-       <input type="radio" name="upload" value="upload" id="upload"> Upload<br>
-        <input type="radio" name="inline" value="inline" id="inline"> Inline<br>
-        <input type="radio" name="online" value="online" id="online"> Online<br>
-        <br>
-        <input type="checkbox" name="requestCIDI" value="Request CINDI Copyright Form" id="requestCIDI">Request CINDI Copyright Form<br>
-        <input type="radio" name="upload1" value="upload" id="upload1"> Upload<br>
-        <input type="radio" name="inline1" value="inline" id="inline1"> Inline<br>
-        <input type="radio" name="online1" value="online" id="online1"> Online<br>
-        <br>
-        <input type="checkbox" name="requestFinal" value="Request Final Version" id="requestFinal">Request Final Version<br>
-        <input type="checkbox" name="requestSlide" value="Request slide" id="requestSlide">Request Slide<br>
-        <input type="checkbox" name="enablePresentation" value="Enable presentation/meeting" id="enablePresentation">Enable Presentation/Meeting<br>
-        <input type="checkbox" name="checkFonts" value="Check Embedded Fonts of PDF file" id="checkFonts">Check Embedded Fonts of PDF file<br>
-        
-         <a href="<?php echo site_url('Event/submit/') ?>" class="btn btn-success" >Add New Event</a>
+          
+         <a href="<?php echo site_url('Event/addEvent/') ?>" class="btn btn-success" >Add New Event</a>
         
         <button type="cancel" class="btn btn-success" >cancel</button>
         
