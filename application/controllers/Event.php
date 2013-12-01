d63a8d2ed3f55f7c11babe8cafc3197ef62d7a0c
Shurouq <sh.abusalah@gmail.com>
2013-12-01 13:07:32 -0500

    getting data from addevent page

--- application/controllers/Event.php
+++ application/controllers/Event.php
@@ -13,4 +13,35 @@ class Event extends CI_Controller {
 		$this->load->view('header');
 		$this->load->view('event_page');
 	}
+	
+	public function addedEvent()
+	{
+		$eventTitle = $this->input->get('eventTitle');
+		$eventDescription = $this->input->get('eventDescription');
+		$idTopicHierarcy = $this->input->get('idTopicHierarchy');
+		$startDate = $this->input->get('startDate');
+		$endDate = $this->input->get('endDate');
+		$schedulingTemplate = $this->input->get('schedulingTemplate');
+		$copyGroupsFrom = $this->input->get('copyGroupsFrom');
+		$requestFullPaper = $this->input->get('requestFullPaper');
+		$requestAbstract = $this->input->get('requestAbstract');
+		$enableRegistration = $this->input->get('enableRegistration');
+		$enablePaperAuction = $this->input->get('enablePaperAuction');
+		$enablePaperReview = $this->input->get('enablePaperReview');
+		$enableAutoReviewAllocation = $this->input->get('enableAutoReviewAllocation');
+		$enableBlindDebate = $this->input->get('enableBlindDebate');
+		$requestPublisherCpoyright = $this->input->get('requestPublisherCpoyright');
+		$upload = $this->input->get('upload');
+		$inline = $this->input->get('inline');
+		$online = $this->input->get('online');
+		$requestCIDI = $this->input->get('requestCIDI');
+		$upload1 = $this->input->get('upload1');
+		$inline1 = $this->input-get('inline1');
+		$online1 = $this->input->get('online1');
+		$requestFinal = $this->input->get('requestFinal');
+		$requestSlide = $this->input->get('requestSlide');
+		$enablePresentation = $this->input->get('enablePresentation');
+		$checkFonts = $this->input->get('checkFonts');
+			
+	}
 }
--- application/views/add_new_event.php
+++ application/views/add_new_event.php
@@ -6,21 +6,21 @@
     
         <div class="form-group">
         <p> Event Title </p>
-        <input type="text" class="form-control" name ="event name" id="eventTitle">
+        <input type="text" class="form-control" name ="eventTitle" id="eventTitle">
         </div>        
         
         
         
         <div class="form-group">
         <p>Event Description</p>
-        <input type="text"  class="form-control" name ="event description" style= "height:200px;" id= "eventDescription">
+        <input type="text"  class="form-control" name ="eventDescription" style= "height:200px;" id= "eventDescription">
         </div>
    
         
         
         <div class="form-group">
         <p>Subject Hierarchy Class </p>
-        <select class="form-control" id="idTopicHierarchy">
+        <select class="form-control" id="idTopicHierarchy" name="idTopicHierarchy">
         <option value = "ACM">ACM</option>
         <option value="??">??</option>
         </select>
@@ -31,19 +31,19 @@
          
          <div class="form-group">
         <p>Start Date: </p>
-        <input type="text" class="form-control" name ="event start date" id="startDate">
+        <input type="text" class="form-control" name ="startDate" id="startDate">
         </div>
        
           <div class="form-group">
         <p>End Date: </p>
-        <input type="text" class="form-control" name ="event end date" id="endDate">        
+        <input type="text" class="form-control" name ="endDate" id="endDate">        
         </div>
         
         
         
         <div class="form-group">
         <p>Scheduling Template </p>
-        <select class="form-control" id="schedulingTemplate">
+        <select class="form-control" id="schedulingTemplate" name="schedulingTemplate">
         <option value = "Use the system default">Use the system default</option>
         <option value="??">??</option>
         </select>
@@ -54,7 +54,7 @@
         
         <div class="form-group">
         <p>Copy Groups From </p>
-        <select class="form-control" id = "copyGroupsFrom">
+        <select class="form-control" id = "copyGroupsFrom" name="copyGroupsFrom">
         <option value = "don't copy">Don't copy</option>
         <option value="??">??</option>
         </select>
@@ -69,30 +69,30 @@
         
         <div class="col-lg-6">
        
-        <input type="radio" name="first" value="Request Full Paper" id="requestFullPaper"> Request Full Paper <br>
+        <input type="radio" name="requestFullPaper" value="Request Full Paper" id="requestFullPaper"> Request Full Paper <br>
     
-        <input type="radio" name="second" value="Request Abstract" id="requestAbstract"> Request Abstract<br>
+        <input type="radio" name="requestAbstract" value="Request Abstract" id="requestAbstract"> Request Abstract<br>
       
-       <input type="checkbox" name="firstcheckbox" value="Enable Registration Control" id="enableRegistration">Enable Registration Control<br>
-        <input type="checkbox" name="secondcheckbox" value="Enable Paper Auction" id="enablePaperAuction">Enable Paper Auction<br>
-        <input type="checkbox" name="thirdcheckbox" value="Enable Paper Review" id="enablePaperReview">Enable Paper Review<br>
-        <input type="checkbox" name="fourthcheckbox" value="Enable Auto Review Allocation" id="enableAutoReviewAllocation">Enable Auto Review Allocation<br>
-        <input type="checkbox" name="fifthcheckbox" value="Enable Blind Debate" id="enableBlindDebate">Enable Blind Debate<br>
-        <input type="checkbox" name="sixthcheckbox" value="Request Publisher Copyright Form" id="requestPublisherCpoyright">Request Publisher Copyright Form<br>
+       <input type="checkbox" name="enableRegistration" value="Enable Registration Control" id="enableRegistration">Enable Registration Control<br>
+        <input type="checkbox" name="enablePaperAuction" value="Enable Paper Auction" id="enablePaperAuction">Enable Paper Auction<br>
+        <input type="checkbox" name="enablePaperReview" value="Enable Paper Review" id="enablePaperReview">Enable Paper Review<br>
+        <input type="checkbox" name="enableAutoReviewAllocation" value="Enable Auto Review Allocation" id="enableAutoReviewAllocation">Enable Auto Review Allocation<br>
+        <input type="checkbox" name="enableBlindDebate" value="Enable Blind Debate" id="enableBlindDebate">Enable Blind Debate<br>
+        <input type="checkbox" name="requestPublisherCpoyright" value="Request Publisher Copyright Form" id="requestPublisherCpoyright">Request Publisher Copyright Form<br>
             
        <input type="radio" name="upload" value="upload" id="upload"> Upload<br>
         <input type="radio" name="inline" value="inline" id="inline"> Inline<br>
         <input type="radio" name="online" value="online" id="online"> Online<br>
         <br>
-        <input type="checkbox" name="seventhcheckbox" value="Request CINDI Copyright Form" id="requestCIDI">Request CINDI Copyright Form<br>
-        <input type="radio" name="upload" value="upload" id="upload1"> Upload<br>
-        <input type="radio" name="inline" value="inline" id="inline1"> Inline<br>
-        <input type="radio" name="online" value="online" id="online1"> Online<br>
+        <input type="checkbox" name="requestCIDI" value="Request CINDI Copyright Form" id="requestCIDI">Request CINDI Copyright Form<br>
+        <input type="radio" name="upload1" value="upload" id="upload1"> Upload<br>
+        <input type="radio" name="inline1" value="inline" id="inline1"> Inline<br>
+        <input type="radio" name="online1" value="online" id="online1"> Online<br>
         <br>
-        <input type="checkbox" name="eighthcheckbox" value="Request Final Version" id="requestFinal">Request Final Version<br>
-        <input type="checkbox" name="ninethcheckbox" value="Request slide" id="requestSlide">Request Slide<br>
-        <input type="checkbox" name="tenthcheckbox" value="Enable presentation/meeting" id="enablePresentation">Enable Presentation/Meeting<br>
-        <input type="checkbox" name="eleventhcheckbox" value="Check Embedded Fonts of PDF file" id="checkFonts">Check Embedded Fonts of PDF file<br>
+        <input type="checkbox" name="requestFinal" value="Request Final Version" id="requestFinal">Request Final Version<br>
+        <input type="checkbox" name="requestSlide" value="Request slide" id="requestSlide">Request Slide<br>
+        <input type="checkbox" name="enablePresentation" value="Enable presentation/meeting" id="enablePresentation">Enable Presentation/Meeting<br>
+        <input type="checkbox" name="checkFonts" value="Check Embedded Fonts of PDF file" id="checkFonts">Check Embedded Fonts of PDF file<br>
         
          <a href="<?php echo site_url('Event/submit/') ?>" class="btn btn-success" >Add New Event</a>
         
