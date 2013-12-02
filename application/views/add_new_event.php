2ae3c80c371e25f59d47c6ade76c65e63a70549d
Shurouq <sh.abusalah@gmail.com>
2013-12-02 17:12:54 -0500

    having same style for create event page

--- application/views/add_new_event.php
+++ application/views/add_new_event.php
@@ -12,25 +12,25 @@
 		<form role="form" class="form-horizontal" name="createEvent" action="<?php echo site_url('Event/submitedEvent/') ?>" >
        
 	   <div class="form-group">
-        <p> Event Title </p>
+        <label> Event Title: </label>
         <input type="text" class="form-control" name ="eventName" id="eventName">
         </div>        
         
         
         
         <div class="form-group">
-        <p>Event Description</p>
+        <label>Event Description: </label>
         <input type="text"  class="form-control" name ="eventDescription" style= "height:200px;" id= "eventDescription">
         </div>
    
         <div class="form-group">
-        <p>Start Date: </p>
+        <label>Start Date: </label>
 		<input type="Text" class="form-control" name="startDate"  id="startDate" value="">
             <a href="javascript:show_calendar('document.createEvent.startDate', document.createEvent.startDate.value);"><img src="../assets/img/cal.gif" width="16" height="16" border="0" alt="Click Here to Pick up the start Time"></a> 
         </div>
        
           <div class="form-group">
-        <p>End Date: </p>
+        <label>End Date: </label>
         <input type="Text" class="form-control" name="endDate"  id="endDate" value="">
             <a href="javascript:show_calendar('document.createEvent.endDate', document.createEvent.endDate.value);"><img src="../assets/img/cal.gif" width="16" height="16" border="0" alt="Click Here to Pick up the start Time"></a>       
         </div>
