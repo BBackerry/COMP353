8aaab2bcb643f24ecbf9d8c6beeec2c336305ce6
Shurouq <sh.abusalah@gmail.com>
2013-12-03 16:12:46 -0500

    view event

--- application/views/event_page.php
+++ application/views/event_page.php
@@ -1,55 +1,87 @@
       <div class="container">
-      
-      <h2 class="panel-title">Event Page</h2>
+	  <div class="row">
+      <br>
+      <h3>Event Page</h3>
       <div class="panel-body">
-        <p>Event for conference:</p>
-        
-        <div class="form-group">
-        <h4 class="panel-title">Event Title </h4>
-      
-        </div>        
-        
-        
-        
-        <div class="form-group">
-        <h4 class="panel-title">Event Description</h4>
+	  
+		<div class="panel panel-default">
+					
+      				 
+		<form role="form" class="form-horizontal" name="viewEvent" action="<?php echo site_url('Event/editEvent/') ?>" >
        
+		
+		<?php foreach($eventDetail as $row): ?> 
+			
+        <div class="form-group">	 
+			<label><b>Event Title: </b><br> <?= $row->eventName ?> </label>		
+        </div>                 
+        
+        <div class="form-group">
+			<label><b> Event Description: </b><br><?= $row->eventDescription ?></label>
         </div>
    
-        
-        
-        <div class="form-group">
-        <h4>Subject Hierarchy Class </h4>
-        
-              
-        </div>
-       
-        
-         
          <div class="form-group">
-        <h4>Start Date: </h4>
-     
+			<label><b>Created By: </b><?= $row->createdBy ?></label>
+        </div>                
+         
+        <div class="form-group">
+			<label><b>Start Date: </b><?= $row->startDate ?></label>
         </div>
        
-          <div class="form-group">
-        <h4>End Date: </h4>
-               
-        </div>
-        
-        
-        
         <div class="form-group">
-        <h4>Scheduling Template </h4>
-        <select class="form-control">
-        <option value = "Use the system default">Use the system default</option>
-        <option value="??">??</option>
-        </select>
-        </div>
+        <label><b>End Date: </b><?= $row->endDate ?></label>               
+        </div>          
+        <?php endforeach; ?>
+       
+	   <?php foreach($EventTopicDetail as $row): ?> 
+			<div class="form-group">	 
+				<label><b>Event Topic(s): </b> <br><?= $row->topicName?> </label>		
+			</div> 	
+			<br>			
+		<?php endforeach; ?>
 	
+		
+		 <?php foreach($meetingDetail as $row): ?> 
+			<div class="form-group">	 
+				<label><b>Event Meeting(s): </b> <br>
+					<?php echo date( "Y-m-d H:i:s", strtotime($row->startTime))?> - <?php echo date( "Y-m-d H:i:s", strtotime($row->endTime)) ?>			
+				</label>	
+			</div> 
+			   <br>
+		<?php endforeach; ?>
+		
+		<?php foreach($phaseTypeDetail as $row): ?>
+		<div class="form-group">
+			<label><b>Phase Date Details: </b></label> 
+			<label> Phase<?php echo $row->idPhase?> </label>
+			<label>:</label>
+			<label> <?php echo $row->phaseName?> </label>
+			
+				</div> 
+			   
+		<?php endforeach; ?>
+			
+		 <?php foreach($phaseDetail as $row): ?> 
+		  
+			<div class="form-group">			
+								
+				<label>Phase Start Date: <?php echo date( "Y-m-d H:i:s", strtotime($row->startTime))?> </label>
+				<label>Phase End Date: <?php echo date( "Y-m-d H:i:s", strtotime($row->endTime)) ?></label>
+					
+			</div> 
+			   
+		<?php endforeach; ?>
+
+		
+		
+		<button style="float:right;" class="btn btn-primary">Edit Event</button>
+		
+		</form>
 		
 		</div>
 		</div>
-	
+		</div>
+		</div>
 	
     <div class="container">
       <!-- Example row of columns -->
@@ -63,6 +95,7 @@
 		<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
 		<script src="<?php echo base_url();?>/assets/js/vendor/bootstrap.min.js"></script>
 		<script src="<?php echo base_url();?>/assets/js/plugins.js"></script>
+		<script src="<?php echo base_url();?>/assets/js/vendor/verify.notify.min.js"></script>
 		<script src="<?php echo base_url();?>/assets/js/main.js"></script>
     </body>
 </html>
