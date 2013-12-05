bdc92c9e9b781178b16e32654f3527b5f68bc7fc
Shurouq <sh.abusalah@gmail.com>
2013-12-04 23:18:08 -0500

    search for paper views

---
+++ application/views/search_papers_result.php
@@ -0,0 +1,36 @@
+    <div class="container">
+	<div class="row">
+		<div class="panel panel-default">
+			<div class="panel-body">
+      				<h3>Search for Paper Result:</h3>
+                  <?php foreach($finalMatchedPapers as $m): ?>
+					
+					<div class="list-group">
+	 +              <a href="<?= site_url('Paper/viewPaper') . '?idPaper' . $paper->idPaper ?>" class="list-group-item">
+	 +                <h4 class="list-group-item-heading"><?= $m->title ?></h4>
+	 +                <p class="list-group-item-text"><?= $m->abstract ?></p>
+	 +              </a>
+	 +            </div>
+ +          <?php endforeach; ?>
+
+			
+			</div>
+		</div>
+	</div>
+      </div>
+
+    <div class="container">
+      <!-- Example row of columns -->
+
+         <footer>
+        <p>&copy; Best Concordia Team</p>
+      </footer>
+    </div> <!-- /container -->
+	
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+		<script src="<?php echo base_url();?>/assets/js/vendor/bootstrap.min.js"></script>
+		<script src="<?php echo base_url();?>/assets/js/plugins.js"></script>
+		<script src="<?php echo base_url();?>/assets/js/main.js"></script>
+    </body>
+</html>
